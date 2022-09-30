b51bda8dd9998492099d553336fbc4e24e94a440
Andras Benei <andras.benei@gmail.com>
2022-09-30 11:48:20 +0200

    Functional, small board

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import pygame
 pygame.font.init()
-import random
+import queue
+
+import logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='C:/Users/andra/OneDrive/Desktop/python/Shortest Path Finder/path_main.log', level=logging.INFO, format='[%(asctime)s] %(levelname)s: %(name)s: #%(lineno)d: %(message)s')
 
 # Window setup:
 
 WIDTH, HEIGHT = 540, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Sudoku')
+pygame.display.set_caption('Shortest Path Finder')
 
 # Colors:
 WHITE = (255, 255, 255)
@@ -14,42 +18,51 @@ BLACK = (0, 0, 0)
 GREY = (128, 128, 128)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
+YELLOW = (255, 255, 0)
 
 # Fonts:
-FONT_CUBE = pygame.font.SysFont('comicsans', 40)
-FONT_GRID = pygame.font.SysFont('comicsans', 40)
+FONT_CUBE = pygame.font.SysFont('serif', 40)
+FONT_GRID = pygame.font.SysFont('serif', 40)
 
-def valid(board, num, pos):
-    # check row
-    for i in range(len(board[0])):
-        if board[pos[0]][i] == num and pos[1] != i:
-            return False
+# -------------------
 
-    # check column
-    for i in range(len(board)):
-        if board[i][pos[1]] == num and pos[0] != i:
-            return False
-
-    # check box
-    box_x = pos[1] // 3
-    box_y = pos[0] // 3
-
-    for i in range(box_y * 3, box_y * 3 + 3):
-        for j in range(box_x * 3, box_x * 3 + 3):
-            if board[i][j] == num and (i, j) != pos:
-                return False
-
-    return True
-
-def find_empty(board):
-
-    for i in range(len(board)):
-        for j in range(len(board[0])):
-            if board[i][j] == 0:
-                return (i, j)
+def find_start(board, start):
+    for i, row in enumerate(board):
+        for j, value in enumerate(row):
+            if value == start:
+                return i, j
 
     return None
 
+def find_end(board, end):
+    for i, row in enumerate(board):
+        for j, value in enumerate(row):
+            if value == end:
+                return i, j
+
+    return None
+
+def find_neighbors(maze, row, col):
+    neighbors = []
+
+    if row > 0:
+        neighbors.append((row - 1, col))
+    if row + 1 < len(maze):
+        neighbors.append((row + 1, col))
+    if col > 0:
+        neighbors.append((row, col - 1))
+    if col + 1 < len(maze[0]):
+        neighbors.append((row, col + 1))
+
+    return neighbors
+
+def draw_game_over(text):
+    draw_text = FONT_GRID.render(text, 1, RED)
+    WIN.blit(draw_text, (WIDTH//2 - draw_text.get_width()//2, HEIGHT//2 - draw_text.get_height()//2))
+    pygame.display.update()
+    pygame.time.delay(5000)
+
+# -------------------------------------------------
 
 class Grid:
 
@@ -57,7 +70,7 @@ class Grid:
     ["#", "O", "#", "#", "#", "#", "#", "#", "#"],
     ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
     ["#", " ", "#", "#", " ", "#", "#", " ", "#"],
-    ["#", " ", "#", " ", " ", " ", "#", " ", "#"],
+    ["#", " ", "#", "#", " ", " ", "#", " ", "#"],
     ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
     ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
     ["#", " ", "#", " ", "#", " ", "#", "#", "#"],
@@ -106,93 +119,72 @@ class Grid:
         self.cubes[row][col].selected = True
         self.selected = (row, col)
 
-    def sketch(self, val):
-        row, col = self.selected
-        self.cubes[row][col].set_temp(val)
-
-    def clear(self):
-        row, col = self.selected
-        if self.cubes[row][col].value == 0:
-            self.cubes[row][col].set_temp(0)
-
-    def place(self, val):
-        row, col = self.selected
-        if self.cubes[row][col].value == 0:
-            self.cubes[row][col].set(val)
-            self.update_model()
-
-            if valid(self.model, val, (row, col)) and self.solve():
-                return True
-            else:
-                self.cubes[row][col].set(0)
-                self.cubes[row][col].set_temp(0)
-                self.update_model()
-                return False
-
     def update_model(self):
         self.model = [[self.cubes[i][j].value for j in range(self.cols)] for i in range(self.rows)]
 
-    def solve(self):
-        
-        find = find_empty(self.model)
-        if not find:
-            return True
-        else:
-            row, col = find
-
-        for i in range(1, 10):
-            if valid(self.model, i, (row, col)):
-                self.model[row][col] = i
-
-                if self.solve():
-                    return True
-                
-                self.model[row][col] = 0
-
-        return False
-
-    def is_finished(self):
-        for i in range(self.rows):
-            for j in range(self.cols):
-                if self.cubes[i][j].value == 0:
-                    return False
-
-        return True
-
     def solve_game(self):
         self.update_model()
-        find = find_empty(self.model)
-        if not find:
-            return True
-        else:
-            row, col = find
+        logger.info(self.model)
+        start = 'O'
+        end = 'X'
+        start_pos = find_start(self.model, start)
+        end_pos = find_start(self.model, end)
 
-        for i in range(1, 10):
-            if valid(self.model, i, (row, col)):
-                self.model[row][col] = i
-                self.cubes[row][col].set(i)
-                self.cubes[row][col].draw_change(True)
-                self.update_model()
-                pygame.display.update()
-                pygame.time.delay(100)
+        q = queue.Queue()
+        q.put((start_pos, [start_pos]))
 
-                if self.solve_game():
-                    return True
+        visited = set()
 
-                self.model[row][col] = 0
-                self.cubes[row][col].set(0)
-                self.update_model()
-                self.cubes[row][col].draw_change(False)
-                pygame.display.update()
-                pygame.time.delay(100)
+        while not q.empty():
+            current_pos, path = q.get()
+            row, col = current_pos
 
-        return False
+            for r, ro in enumerate(self.model):
+                for c, co in enumerate(ro):
+                    if (r, c) in path:
+                        self.cubes[r][c].draw_change('yellow')
+                        self.update_model()
+
+            pygame.display.update()
+            pygame.time.delay(300)
+     
+            if current_pos == end_pos:
+                self.draw_shortest_path('Shortest path', path)
+                return
+
+            neighbors = find_neighbors(self.model, row, col)
+            for neighbor in neighbors:
+                if neighbor in visited:
+                    continue
+
+                r, c = neighbor
+                if self.model[r][c] == '#':
+                    continue
+
+                new_path = path + [neighbor]
+                q.put((neighbor, new_path))
+                visited.add(neighbor)
+
+        draw_game_over('Unsolvable')
+
+    def draw_shortest_path(self, text, path):
+        for r, ro in enumerate(self.model):
+                    for c, co in enumerate(ro):
+                        if (r, c) in path:
+                            self.cubes[r][c].draw_change('green')
+                            self.update_model()
+
+        draw_text = FONT_GRID.render(text, 1, RED)
+        WIN.blit(draw_text, (WIDTH//2 - draw_text.get_width()//2, HEIGHT//2 - draw_text.get_height()//2))
+        pygame.display.update()
+        pygame.time.delay(5000)
+
 
 class Cube:
 
     def __init__(self, value, row, col, width, height):
         self.value = value
-        self.temp = 0
+        # self.temp = 0
         self.row = row
         self.col = col
         self.width = width
@@ -204,36 +196,29 @@ class Cube:
         x = self.col * gap
         y = self.row * gap
 
-        if self.temp != 0 and self.value == 0:
-            text = FONT_CUBE.render(str(self.temp), 1, GREY)
-            WIN.blit(text, (x + 5, y + 5))
-        elif not(self.value == 0):
-            text = FONT_CUBE.render(str(self.value), 1, BLACK)
-            WIN.blit(text, (x + (gap // 2 - text.get_width() // 2), y + (gap // 2 - text.get_height() // 2)))
+        text = FONT_CUBE.render(str(self.value), 1, BLACK)
+        WIN.blit(text, (x + (gap // 2 - text.get_width() // 2), y + (gap // 2 - text.get_height() // 2)))
 
         if self.selected:
             pygame.draw.rect(WIN, BLACK, (x, y, gap, gap), 6)
 
-    def set_temp(self, val):
-        self.temp = val
-
     def set(self, val):
         self.value = val
 
-    def draw_change(self, win=True):
+    def draw_change(self, color='white'):
         gap = self.width // 9
         x = self.col * gap
         y = self.row * gap
 
-        pygame.draw.rect(WIN, WHITE, (x, y, gap, gap), 0)
+        if color == 'green':
+            pygame.draw.rect(WIN, GREEN, (x, y, gap, gap), 0)
+        elif color == 'yellow':
+            pygame.draw.rect(WIN, YELLOW, (x, y, gap, gap), 0)
+        else:
+            pygame.draw.rect(WIN, WHITE, (x, y, gap, gap), 0)
 
         text = FONT_CUBE.render(str(self.value), 1, BLACK)
         WIN.blit(text, (x + (gap // 2 - text.get_width() // 2), y + (gap // 2 - text.get_height() // 2)))
-
-        if win:
-            pygame.draw.rect(WIN, GREEN, (x, y, gap, gap), 3)
-        else:
-            pygame.draw.rect(WIN, RED, (x, y, gap, gap), 3)
 
 def draw_window(board):
     WIN.fill(WHITE)
@@ -241,7 +226,11 @@ def draw_window(board):
     board.draw()
     
 def main():
-    board = Grid(9, 9, 540, 540)
+    rows = 9
+    cols = 9
+    width = 540
+    height = 540
+    board = Grid(rows, cols, width, height)
     key = None
     run = True
 
@@ -250,60 +239,16 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_1:
-                    key = 1
-                if event.key == pygame.K_2:
-                    key = 2
-                if event.key == pygame.K_3:
-                    key = 3
-                if event.key == pygame.K_4:
-                    key = 4
-                if event.key == pygame.K_5:
-                    key = 5
-                if event.key == pygame.K_6:
-                    key = 6
-                if event.key == pygame.K_7:
-                    key = 7
-                if event.key == pygame.K_8:
-                    key = 8
-                if event.key == pygame.K_9:
-                    key = 9
-                if event.key == pygame.K_KP1:
-                    key = 1
-                if event.key == pygame.K_KP2:
-                    key = 2
-                if event.key == pygame.K_KP3:
-                    key = 3
-                if event.key == pygame.K_KP4:
-                    key = 4
-                if event.key == pygame.K_KP5:
-                    key = 5
-                if event.key == pygame.K_KP6:
-                    key = 6
-                if event.key == pygame.K_KP7:
-                    key = 7
-                if event.key == pygame.K_KP8:
-                    key = 8
-                if event.key == pygame.K_KP9:
-                    key = 9
-                if event.key == pygame.K_DELETE:
-                    board.clear()
-                    key = None
 
                 if event.key == pygame.K_SPACE:
                     board.solve_game()
 
                 if event.key == pygame.K_RETURN:
                     i, j = board.selected
-                    if board.cubes[i][j].temp != 0:
-                        if board.place(board.cubes[i][j].temp):
-                            print('Success')
-                        else:
-                            print('Wrong')
-                        key = None
-
-                    if board.is_finished():
-                        print('Game over')
+                    if board.cubes[i][j].value == '#':
+                        board.cubes[i][j].value = ' '
+                    elif board.cubes[i][j].value == ' ':
+                        board.cubes[i][j].value = '#'
             
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
@@ -311,9 +256,6 @@ def main():
                 if clicked:
                     board.select(clicked[0], clicked[1])
                     key = None
-
-        if board.selected and key != None:
-            board.sketch(key)
 
         draw_window(board)
         pygame.display.update()
